147591dd6b5c127e62733297160db3f95b8a319c
rockydey <coderrocky25@gmail.com>
2022-05-01 00:30:37 +0600

    collect all data using get api from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,16 +14,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@laptopwarehousemanageme.n3f6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         const productsCollection = client.db("laptopWarehouse").collection('laptops');
-//     }
-//     finally {
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db("gadgetHouse").collection("items");
 
-//     }
-// }
-// run().catch(console.dir);
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 // Root API
 app.get('/', (req, res) => {
