a0ad4469bb05ec55c1d37a8b25c2100d1a27d37b
rockydey <coderrocky25@gmail.com>
2022-04-30 09:09:32 +0600

    connect mongodb with server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@laptopwarehousemanageme.n3f6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// async function run() {
+//     try {
+//         await client.connect();
+//         const productsCollection = client.db("laptopWarehouse").collection('laptops');
+//     }
+//     finally {
+
+//     }
+// }
+// run().catch(console.dir);
 
 // Root API
 app.get('/', (req, res) => {
