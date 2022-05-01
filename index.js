292f27fcf0f207c086423a8cca811afc17fe2760
rockydey <coderrocky25@gmail.com>
2022-05-01 10:03:11 +0600

    get single item using get api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,13 @@ async function run() {
             const cursor = productsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        });
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await productsCollection.findOne(query);
+            res.send(item);
         });
     }
     finally {
